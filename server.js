474f9b593b492f43e7020021254771a1a7f0cf7e
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-13 13:00:36 -0600

    added database queries for all candidates crud operations

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
@@ -14,9 +14,35 @@ const db = mysql.createConnection({
     database: 'election'
 }, console.log('Connected to election database'));
 
+//get all candidates
 db.query(`SELECT * FROM candidates`,(err, rows) => {
     console.log(rows);
 });
+
+// //get specific candidate
+db.query(`SELECT * FROM candidates WHERE id=1`,(err,row) => {
+    if(err)
+        console.log(err);
+    else console.log(row);
+});
+
+// //delete record from database
+db.query(`DELETE FROM candidates WHERE id = ?`,2,(err,result) => {
+    if(err)
+        console.log(err);
+    else console.log(row);
+});
+
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?, ?, ?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql,params,(err,result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
