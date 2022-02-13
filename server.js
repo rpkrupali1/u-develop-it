2e92fe5e1fc930500da781ce9fcc40c584b87275
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-13 11:05:03 -0600

    added database connection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Ib!26december',
+    database: 'election'
+}, console.log('Connected to election database'));
+
+db.query(`SELECT * FROM candidates`,(err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
